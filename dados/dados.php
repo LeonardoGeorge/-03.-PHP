9955bf9a0f129135682c6f5f24be8a9a950a519a
Leonardo George <leonardogeorge30@gmail.com>
2025-05-23 14:03:12 -0300

    update

--- dados/dados.php
+++ dados/dados.php
@@ -1,5 +1,6 @@
 <?php
 // Simulação de idetificação de redimencionamento.
 // Simulação de enlace da campanha 3
-
+$numero_campanha = $_GET['campanha'];
+echo "<h1>Você veio da campanha $numero_campanha</h1>";
 ?>
